187c6098322fc253b63c3c736ee47355b09194c6
段福 <duanfu456@163.cm>
2025-08-13 00:03:55 +0800

    Bump version to 0.1.8 for configuration management enhancements

--- minispark/__init__.py
+++ minispark/__init__.py
@@ -2,26 +2,22 @@
 MiniSpark - 一个轻量级的Python数据处理库
 """
 
+__version__ = "0.1.8"
+
 from .minispark import MiniSpark
 from .connectors.csv_connector import CSVConnector
 from .connectors.excel_connector import ExcelConnector
 from .connectors.json_connector import JSONConnector
+from .connectors.mysql_connector import MySQLConnector
 from .connectors.sqlite_connector import SQLiteConnector
 from .connectors.duckdb_connector import DuckDBConnector
-from .connectors.mysql_connector import MySQLConnector
-from .connectors.base import BaseConnector
-
-__version__ = "0.1.6"
-__author__ = "段福"
-__email__ = "duanfu456@163.cm"
 
 __all__ = [
     "MiniSpark",
-    "CSVConnector", 
+    "CSVConnector",
     "ExcelConnector",
     "JSONConnector",
-    "SQLiteConnector",
-    "DuckDBConnector",
     "MySQLConnector",
-    "BaseConnector"
+    "SQLiteConnector",
+    "DuckDBConnector"
 ]
--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ with open("CHANGELOG.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="minispqrk",
-    version="0.1.6",
-    author="duanfu",
+    version="0.1.8",
+    author="段福",
     author_email="duanfu456@163.com",
     description="一个轻量级的Python库，用于从多种数据源读取数据并在本地进行高效处理，类似于Apache Spark的功能",
     long_description=long_description,
@@ -46,6 +46,6 @@ setup(
     },
     include_package_data=True,
     package_data={
-        "minispark": ["../config.toml"],
+        "minispark": ["config.toml"],
     },
 )
